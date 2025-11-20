de19a9de04cffbd97fb115fdb34209195cead355
pradeepbadiger09 <pradeepbadiger618@gmail.com>
2025-11-20 16:33:05 +0530

    Create details.py

---
+++ details.py
@@ -0,0 +1,15 @@
+impot sys
+if len(sys.argv) == 3;
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("User provided input valus:")
+else:
+    scrip_name = sys.argy[0]
+    name = "Pradeep"
+    rollno = "125"
+    print("No input give - using default values:")
+
+print("Script Name:", scrip-name)
+print("Student Name:", name)
+print("Roll Number:", rollno)
